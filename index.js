43d9236d3fa70ed15f4b0c25e4570665404282fc
Dipesh <kdipesh2707@gmail.com>
2023-05-17 07:54:14 +0000

    find date

---
+++ index.js
@@ -0,0 +1,9 @@
+const addDays = require("date-fns/addDays");
+
+let dateoftheyear = (days) => {
+  let date1 = addDays(new Date(2020, 08, 22), days);
+  console.log(
+    `$date1.getDate()}-${date1.getMonth() + 1}-${date1.getFullYear()}`
+  );
+};
+module.exports = dateoftheyear;
