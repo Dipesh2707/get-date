08444a66217d17b1e8cd1da33d3021db76c00f88
Dipesh2707 <131431176+Dipesh2707@users.noreply.github.com>
2023-07-15 11:17:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const addDays = require("date-fns/addDays");
 
 let dateoftheyear = (days) => {
-  let date1 = addDays(new Date(2020, 08, 22), days);
+  let date1 = addDays(new Date(2020, 07, 22), days);
   console.log(
     `$date1.getDate()}-${date1.getMonth() + 1}-${date1.getFullYear()}`
   );
